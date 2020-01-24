3b3317a8592c3af5e6dc7c1f8bd4173d55d1e80b
Barbara Mayfield <bmayfield1991@gmail.com>
2020-01-23 19:47:37 -0600

    initial migration table and seeds ran, spell-model, test file and router created

--- data/migrations/20200123181501_initial.js
+++ data/migrations/20200123181501_initial.js
@@ -1,8 +1,11 @@
 
-exports.up = function(knex) {
-  
+exports.up = async function(knex) {
+    await knex.schema.createTable("spells", (table) => {
+        table.increments()
+        table.string("name", 255).notNullable()
+    })
 };
 
-exports.down = function(knex) {
-  
+exports.down = async function(knex) {
+  await knex.schema.dropTableIfExists("spells")
 };
