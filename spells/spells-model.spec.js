e604eca7d3b06179d60ef058cbf7b8f517a133e1
Barbara Mayfield <bmayfield1991@gmail.com>
2020-01-24 12:17:40 -0600

    All subject model tests coming back passing

--- spells/spells-model.spec.js
+++ spells/spells-model.spec.js
@@ -15,4 +15,22 @@ describe("spells model", () => {
         const res = await spellsModel.findById(1)
         expect(res.name).toBe("fireball")
     })
+
+    test("insert", async () => {
+        await spellsModel.insert({ name: "rejuvenation" })
+        const spells = await db("spells").select()
+        expect(spells).toHaveLength(5)
+    })
+
+    test("update", async () => {
+        await spellsModel.update(1, { name: "fire blast" })
+        const spell = await spellsModel.findById(1)
+        expect(spell.name).toBe("fire blast")
+    })
+
+    test("remove", async () => {
+        await spellsModel.remove(1)
+        const spells = await spellsModel.getSpells()
+        expect(spells).toHaveLength(3)
+    })
 })
